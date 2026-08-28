626c167ce35f782da263908ffac8d3a3e0a458f2
DrRedBird <92159407+DrRedBird@users.noreply.github.com>
2026-08-28 12:44:41 +0300

    Prepare v3.0.2-beta.2 prerelease

--- src/build-info.ts
+++ src/build-info.ts
@@ -1,2 +1,2 @@
-export const BUILD_LABEL = "ThemeDeck Build 2026-08-21 17:53:27 EDT";
-export const BUILD_ID = "2026-08-21-175327-EDT";
+export const BUILD_LABEL = "ThemeDeck Build 2026-08-28 05:42:05 EDT";
+export const BUILD_ID = "2026-08-28-054205-EDT";
